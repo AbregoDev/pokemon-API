20052e2dec415bb33dd5e554636dbc914d5a2c0d
Bryan Moreno <abre.go@outlook.com>
2021-09-02 20:54:30 -0500

    body parser now is built in on express

--- app.js
+++ app.js
@@ -2,10 +2,9 @@
 const express = require('express');
 const app = express();
 
-//Body Parser
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// Body Parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use('/v1', require('./routes'));
 
