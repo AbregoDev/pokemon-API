97313e56385301934368e1244c95264138d471df
Bryan Moreno <abre.go@outlook.com>
2021-09-17 21:05:48 -0500

    Fix Pokemon controller and model

--- controllers/pokemons.js
+++ controllers/pokemons.js
@@ -2,65 +2,84 @@ const mongoose = require('mongoose')
 const Pokemon = mongoose.model("Pokemon")
 
 const createPokemon = (req, res, next) => {
-	let pokemon = new 	Pokemon(req.body);
-	pokemon.save().then(pok =>{
-		res.status(200).send(pok)
-	}).catch(next)
+    const pokemon = new Pokemon(req.body);
+    pokemon.save()
+    .then(pok => {
+        res.status(200).send(pok)
+    })
+    .catch(next);
 }
 
-function getPokemon (req, res, next) {
-	if (req.params.id){
-		Pokemon.findById(req.params.id)
-		.then(pok => {res.send(pok)})
-		.catch(next)
-	} else {
-		Pokemon.find()
-		.then(pokemons => {res.send(pokemons)})
-		.catch(next)
-	}
+function getPokemon(req, res, next) {
+    if (req.params.id) {
+        Pokemon.findOne({ pokedexNumber: req.params.id})
+            .then(pok => { 
+                if(pok) {
+                    res.status(200).send(pok);
+                } else {
+                    res.status(404).send('No se ha encontrado');
+                }
+            })
+            .catch(next)
+    } else {
+        Pokemon.find()
+            .then(pokemons => {
+                res.send(pokemons)
+            }).catch(next)
+    }
 }
 
 const modifyPokemon = (req, res, next) => {
-	Pokemon.findById(req.params.id)
-	.then(pokemon =>{
-		if(!pokemon){ 
-			return res.sendStatus(401)
-		}
-		let nuevaInfo = req.body
+    Pokemon.findOne({ pokedexNumber: req.params.id })
+        .then(pokemon => {
+            if (!pokemon) {
+                return res.sendStatus(404);
+            }
+            
+            const nuevaInfo = req.body;
 
-		if (typeof nuevaInfo.pokedexNumber !== "undefined") {
-			pokemon.pokedexNumber = nuevaInfo.pokedexNumber
-		}
-		if (typeof nuevaInfo.name !== "undefined") {
-			pokemon.name = nuevaInfo.name
-		}
-		if (typeof nuevaInfo.imageUrl !== 'undefined'){
-			pokemon.imageUrl = nuevaInfo.imageUrl
-		}
-		if (typeof nuevaInfo.type !== 'undefined'){
-			pokemon.type = nuevaInfo.type
-		}
-		if (typeof nuevaInfo.abilities !== 'undefined'){
-			pokemon.abilities = nuevaInfo.abilities
-		}
-		if (typeof nuevaInfo.category !== 'undefined'){
-			pokemon.category = nuevaInfo.category
-		}
-		pokemon.save().then(updated => {
-			res.status(201).json(updated.publicData())
-		}).catch(next)
-	}).catch(next)
+            if (nuevaInfo.pokedexNumber) {
+                pokemon.pokedexNumber = nuevaInfo.pokedexNumber
+            }
+            if (nuevaInfo.name) {
+                pokemon.name = nuevaInfo.name
+            }
+            if (nuevaInfo.gen) {
+                pokemon.gen = nuevaInfo.gen
+            }
+            if (nuevaInfo.imageUrl) {
+                pokemon.imageUrl = nuevaInfo.imageUrl
+            }
+            if (nuevaInfo.types) {
+                pokemon.types = nuevaInfo.types
+            }
+            if (nuevaInfo.abilities) {
+                pokemon.abilities = nuevaInfo.abilities
+            }
+            if (nuevaInfo.category) {
+                pokemon.category = nuevaInfo.category
+            }
+            
+            pokemon.save()
+                .then(updated => {
+                    res.status(201).json(updated.publicData());
+                })
+                .catch(next);
+        }).catch(next)
 }
 
 const deletePokemon = (req, res, next) => {
-	Pokemon.findOneAndDelete({_id: req.params.id})
-	.then(r => {res.status(200).send(`The Pokémon with id ${req.params.id} has been deleted successfully`)})
-	.catch(next)
+    Pokemon.findOneAndDelete({ pokedexNumber: req.params.id })
+        .then(r => {
+            res.status(200)
+                .send(`The Pokémon with Pokedex id ${req.params.id} has been deleted successfully`);
+        })
+        .catch(next);
 }
 
 module.exports = {
-	getPokemon,
-	createPokemon,
-	modifyPokemon,
-	deletePokemon,
+    getPokemon,
+    createPokemon,
+    modifyPokemon,
+    deletePokemon,
 };
--- models/Pokemon.js
+++ models/Pokemon.js
@@ -1,44 +1,26 @@
-// class Pokemon {
-
-//     constructor(
-//         pokedexNumber,
-//         name,
-//         imageUrl,
-//         type,
-//         abilities,
-//         category) {
-//             this.pokedexNumber = pokedexNumber;
-//             this.name = name;
-//             this.imageUrl = imageUrl;
-//             this.type = type;
-//             this.abilities = abilities;
-//             this.category = category;
-//     }
-// }
-
-// module.exports = Pokemon;
-
 const mongoose = require("mongoose");
 
 const PokemonSchema = new mongoose.Schema({
-  pokedexNumber: {type: Number, require: true},   
-  name: {type: String, required: true},
-  imageUrl: {type: String},
-  type: {type: String},
-  abilities:{type: String},
-  category: { type: String },
-}, { timestamps: true , collection : 'pokemons'}) //Cambiar coleccion a la misma de la base de datos disponible
+    pokedexNumber: { type: Number, required: true },
+    name: { type: String, required: true },
+    gen: { type: Number },
+    imageUrl: { type: String },
+    types: [String],
+    abilities: [String],
+    category: { type: String },
+}, { collection: 'Pokemon' });
 
 PokemonSchema.methods.publicData = function () { //No cambiar a arrow por que se rompe xD
-  return {
-    id: this.id,
-    pokedexNumber: this.pokedexNumber,
-    name: this.name,
-    imageUrl: this.imageUrl,
-    type: this.type,
-    abilities: this.abilities,
-    category: this.category
-  };
+    return {
+        id: this.id,
+        pokedexNumber: this.pokedexNumber,
+        name: this.name,
+        gen: this.gen,
+        imageUrl: this.imageUrl,
+        types: this.types,
+        abilities: this.abilities,
+        category: this.category
+    };
 };
 
 mongoose.model('Pokemon', PokemonSchema)
