85b0f452b20286da2f593785ea1c78f9ad9dda75
EnriqueHM <enriqhz@gmail.com>
2021-09-23 21:47:43 -0500

    Configuracion de passport

---
+++ config/passport.js
@@ -0,0 +1,22 @@
+
+// Se importa passport, que es un middleware para autenticación.
+const passport = require('passport');    
+
+// Se importa estrategia autenticación. --> passport-local
+const LocalStrategy = require('passport-local').Strategy;   
+const mongoose = require('mongoose');
+const Usuario = mongoose.model('Usuario');
+
+
+// Se configuran los elementos utilizados para habilitar sesión.
+passport.use(new LocalStrategy({                            
+    usernameField: 'email',
+    passwordField: 'password'
+}, function (email, password, done) {
+    Usuario.findOne({ email: email }).then(function (user) {
+    if (!user || !user.validarPassword(password)) {
+      return done(null, false, { errors: { 'email o contraseña': 'equivocado(a)' } });
+    }
+    return done(null, user);
+  }).catch(done);
+}));
