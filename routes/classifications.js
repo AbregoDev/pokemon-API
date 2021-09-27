ec698f9a952107c41e1b5c0aeb0592b537c7bc5a
Bryan Moreno <abre.go@outlook.com>
2021-09-26 20:12:49 -0500

    auth classification route

--- routes/classifications.js
+++ routes/classifications.js
@@ -8,11 +8,13 @@ const {
     countClassification,
 } = require('../controllers/classification');
 
+const auth = require('./auth');
+
 router.get('/', getClassification);
 router.get('/count', countClassification);
 router.get('/:id', getClassification)
-router.post('/', createClassification);
-router.put('/:id', modifyClassification);
-router.delete('/:id', deleteClassification);
+router.post('/', auth.requerido, createClassification);
+router.put('/:id', auth.requerido, modifyClassification);
+router.delete('/:id', auth.requerido, deleteClassification);
 
 module.exports = router;
